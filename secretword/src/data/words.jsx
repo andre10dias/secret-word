a5eccd96c9b704151631522a47358ccddb2f3013
andre10dias <andre10dias@ymail.com>
2025-11-10 19:59:22 -0300

    Ajustes gerais

---
+++ secretword/src/data/words.jsx
@@ -0,0 +1,14 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Laranja", "Uva", "Manga"],
+    corpo: ["Braço", "Perna", "Coração", "Cérebro", "Olho"],
+    computador: ["Teclado", "Mouse", "Monitor", "Placa-mãe", "Processador"],
+    programação: ["JavaScript", "Python", "Java", "C++", "Ruby"],
+    alimento: ["Arroz", "Feijão", "Carne", "Salada", "Pão"],
+    bebida: ["Água", "Suco", "Refrigerante", "Café", "Chá"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Vôlei"],
+    país: ["Brasil", "Argentina", "Canadá", "Japão", "Alemanha"],
+    cidade: ["São Paulo", "Rio de Janeiro", "Nova York", "Tóquio", "Londres"],
+    animal: ["Cachorro", "Gato", "Elefante", "Leão", "Tigre"],
+    cor: ["Vermelho", "Azul", "Verde", "Amarelo", "Preto"],
+}
