d3d39dd418ce15edf492380b8f3e0695e5187812
andre10dias <andre10dias@ymail.com>
2025-11-20 11:29:04 -0300

    Escolhendo palavra e categoria aleatoriamente

--- secretword/src/App.jsx
+++ secretword/src/App.jsx
@@ -15,8 +15,38 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    // Escolhe uma categoria aleatória
+    const categories = Object.keys(words);
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // Escolhe uma palavra aleatória da categoria escolhida
+    const word = 
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    // console.log(category);  
+    // console.log(word);
+    return { word, category };
+  }
+
   // Inicia palavra secreta do jogo
   const startGame = () => {
+    const { word, category } = pickedWordAndCategory();
+    
+    // Cria um array de letras
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    // Atualiza os estados
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+    
     setGameStage(stages[1].name);
   }
 
